f44acab903701a610972110e4e4ae1377b0e30f1
John Neal <jsneal@bu.edu>
2018-09-12 19:44:34 -0400

    Fixed URL bug

--- API_Mini_Project/Twitter_API_Call.py
+++ API_Mini_Project/Twitter_API_Call.py
@@ -4,6 +4,14 @@
 ## and prints the media of these statuses. 
 ## It is an initial file to get off the ground running with this project
 import twitter
+import requests
+from io import BytesIO
+from PIL import Image
+
+""".request
+with urllib.request.urlopen('') as response:
+   html = response.read()"""
+
 
 twitter_api = twitter.Api(consumer_key='aq3WXwQGtK1NllOV0eXGhPfuY',
                           consumer_secret='7CNOFhFNdVZ6AOUZDCc6C2aaiiMZGLnmU34PgfMZwwdQnHUwTf',
@@ -12,7 +20,26 @@ twitter_api = twitter.Api(consumer_key='aq3WXwQGtK1NllOV0eXGhPfuY',
 print(twitter_api.VerifyCredentials())
 print("\n\n\n")
 sirjoancornella_statuses = twitter_api.GetUserTimeline(screen_name="sirjoancornella")
+counter = 0
 for status in sirjoancornella_statuses:
-	media_List = []
-	medialist.append(status.media)
-	print(status.media)
+	if type(status.media) is list:
+		print(status.media[0].display_url)
+
+"""		response = requests.get(status.media[0].url)
+		img = Image.open(BytesIO(response.content))
+		print(type(img))"""
+		
+
+
+"""
+	print(type(status.media))
+	counter = counter+1
+	print(counter)
+	media_list = []
+	tweet = []
+	tweet.append(status.media)
+	media_list.append(tweet)
+print("---------------------------")
+for tweets in media_list:
+	for status in media_list:
+		print(status[0])"""
