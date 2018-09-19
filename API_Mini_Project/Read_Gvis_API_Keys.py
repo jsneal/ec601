723da97a6dbd1119b507de312938e156aa5d6679
John Neal <jsneal@bu.edu>
2018-09-19 01:11:37 -0400

    Adding two modules related to Google Vision:
    Getting credentials and getting annotations of images

---
+++ API_Mini_Project/Read_Gvis_API_Keys.py
@@ -0,0 +1,5 @@
+def Read_Gvis_API_Keys():
+    from google.oauth2 import service_account       # From Google documentation https://google-auth.readthedocs.io/en/latest/user-guide.html#service-account-private-key-files
+    credentials = service_account.Credentials.from_service_account_file(
+    '/home/jsneal/Desktop/API_Keys/Google Cloud/Mini_Project_GVision_Keys.json')
+    return credentials
--- API_Mini_Project/main.py
+++ API_Mini_Project/main.py
@@ -1,5 +1,10 @@
 from Read_API_Keys import Read_API_Keys
 from Twitter_API_Call import Twitter_API_Call
+from Read_Gvis_API_Keys import Read_Gvis_API_Keys
+from GoogleVis_API_Call import GoogleVis_API_Call
 
 api_access_info = Read_API_Keys()
 Twitter_API_Call(api_access_info)
+client = Read_Gvis_API_Keys()
+GoogleVis_API_Call(client)
+
