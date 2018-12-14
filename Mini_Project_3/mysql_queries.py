87720c0a7c377595e4baadaad1e95e13ce200e44
John Neal <jsneal@bu.edu>
2018-12-14 12:38:06 -0500

    added mysql_queries module which will execute the queries (not integrated with main_mysql.py yet)

---
+++ Mini_Project_3/mysql_queries.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import sys
+
+try:
+	file = open('/home/jsneal/Desktop/API_Keys/MySQL/password.txt', 'r') # The file where my API keys were stored.
+	password = file.readline()
+	cnx = mysql.connector.connect(user='root', password=password,
+                            host='127.0.0.1',
+                            database='Mini_Project_3'
+                            )
+	cursor = cnx.cursor()
+	try:	# need this if an image doesn't get labels
+		query = "SELECT DISTINCT TAG, COUNT(*) FROM IMAGE_TAGS GROUP BY TAG ORDER BY COUNT(*) DESC"
+		cursor.execute(query)
+
+		for field in cursor:
+
+			print(field)
+
+	except:
+		print("Query failed!")
+
+	cnx.commit()
+
+	cursor.close()
+	cnx.close()
+	print('MySQL query executed!')
+except:
+	print(sys.exc_info())
+	print('mysql_queries error!')
